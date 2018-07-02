931f2a4d9595eb2b944cff6f9cae0dabe5476a66
Anıl Aynacı <anil.aynaci@hotmail.com>
2018-07-02 15:20:07 +0300

    userIcon added and set view of userProfile span

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,16 @@
 function bodyOnLoad() {
     var loginSpan = document.getElementById("loginSpan");
     var logoutSpan = document.getElementById("logoutSpan");
+    var profileSpan = document.getElementById("profileSpan");
 
     if (isLoggedInFunc()) {
         loginSpan.style.display = "none";
         logoutSpan.style.display = "block";
+        profileSpan.style.display = "block";
     } else {
         loginSpan.style.display = "block";
         logoutSpan.style.display = "none";
+        profileSpan.style.display = "none";
     }
 }
 
